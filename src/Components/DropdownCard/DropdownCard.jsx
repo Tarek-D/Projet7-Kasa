f656eef2fef07caaafafaa3f3f4f3a039b67019a
Tarek Noubli <tareknoubli@gmail.com>
2022-12-22 21:11:27 +0100

    DropdownCard jsx and css

---
+++ src/Components/DropdownCard/DropdownCard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./dropdownCard.css";
+import arrowUp from "../../assets/dropdownCardArrows/arrow-up.svg";
+import arrowDown from "../../assets/dropdownCardArrows/arrow-down.svg";
+
+function DropdownCard({ title, description }) {
+  const [isOpen, setisOpen] = useState(false);
+
+  const handleToggle = (e) => {
+    setisOpen(!isOpen);
+  };
+
+  return (
+    <div className="card-dropdown">
+      <div className="dropdown-card-header" onClick={handleToggle}>
+        <span className="dropdown-card-title">{title}</span>
+        <div className="btn-arrow">
+          {isOpen ? (
+            <img src={arrowUp} alt="up arrow" />
+          ) : (
+            <img src={arrowDown} alt="down arrow" />
+          )}
+        </div>
+      </div>
+      {isOpen && (
+        <div className="dropdown-card-body">
+          <p className="dropdown-card-description">{description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DropdownCard;
