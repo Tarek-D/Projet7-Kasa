4bf0a5d54b79137ceaca75986e3dd1dad3add423
Tarek Noubli <tareknoubli@gmail.com>
2022-12-22 21:11:43 +0100

    Footer component jsx and css

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/LOGO.svg";
+
+function Footer() {
+  return (
+    <footer>
+        <img src={logo}/>
+        <span className="footer-text">© 2020 Kasa. All rights reserved</span>
+    </footer>
+  );
+}
+
+export default Footer;
