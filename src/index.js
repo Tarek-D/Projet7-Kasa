c15c43224c7762eb3a071ec1d3a4e0d6175c28ae
Tarek Noubli <tareknoubli@gmail.com>
2023-01-02 13:09:40 +0100

    Update index.js
    remove of typeface and import index css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'typeface-montserrat/index.css';
+import './style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
