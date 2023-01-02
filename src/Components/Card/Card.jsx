25d50f1113893e0855f12b86e7fb30b052ee8c07
Tarek Noubli <tareknoubli@gmail.com>
2023-01-02 13:03:24 +0100

    Update Card.jsx
    add img alt personnalized

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card({ id, image, title }) {
   return (
     <Link to={`/AppartmentDetail/${id}`} state ={id}>
       <div className="card" id={id}>
-        <img src={image} alt="" />
+        <img src={image} alt={"Image " + title } />
         <p className="card-title">{title}</p>
       </div>
     </Link>
