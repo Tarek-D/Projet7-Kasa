d079cb9574bbf582e112417bca786cd37002a083
Tarek Noubli <tareknoubli@gmail.com>
2023-01-02 13:10:44 +0100

    Update AppartmentDetail.jsx
    change logic to display an error page if id is not founded in data

--- src/pages/AppartmentDetail/AppartmentDetail.jsx
+++ src/pages/AppartmentDetail/AppartmentDetail.jsx
@@ -1,7 +1,5 @@
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useEffect, useRef } from "react";
+import { useEffect,  useState } from "react";
 import DropdownCard from "../../Components/DropdownCard/DropdownCard.jsx";
 import Gallery from "../../Components/Gallery/Gallery.jsx";
 import Tag from "../../Components/Tag/Tag.jsx";
@@ -9,8 +7,51 @@ import appartments from "../../data/appartments.json";
 import "./appartmentDetail.css";
 import Rate from "../../Components/Rate/Rate.jsx";
 import Host from "../../Components/Host/Host.jsx";
+import Error from "../Error/Error";
 
 function AppartmentDetail() {
+
+  const [logement, setLogement] = useState({
+    tags: [],
+    equipments: [],
+    pictures: "",
+    rating: "",
+    host: { name: "", picture: "" },
+    location: ""
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    appartments.map((house) => {
+      if (house.id === id) {
+        setLogement(house);
+      }
+      return null;
+    });
+  }, [id])
+
+  const equipments = logement.equipments.map((equipment) => (
+    <>
+      {equipment}
+      <br />
+    </>
+  ));
+
+  if (logement.title === undefined){
+    return <Error />
+  }
+
+
+
+  /*
+  let mapEquipement = logement.equipments.map((l, index) => {
+    <><p> {logement.equipments[index]} </p> <br/></>
+  })
+  */
+
+
+  /*
   const location = useLocation() || "default-id";
   const navigate = useNavigate();
   const id = location.state;
@@ -30,42 +71,39 @@ function AppartmentDetail() {
   if (!appartment) {
     return null;
   }
-  const equipments = appartment.equipments.map((equipment, index) => (
-    <>
-      {equipment}
-      <br />
-    </>
-  ));
-  
+ 
+  */
+
   return (
     <main>
-    <div>
-      <Gallery images={appartment.pictures} />
-    <div className="appartment-details">
       <div>
-        <h1 className="appartment-title">{appartment.title}</h1>
-        <span className="location-text">{appartment.location}</span>
-        <div className="tag-list">
-          {appartment.tags.map((tag, index) => (
-            <Tag title={tag} key={index}/>
-          ))}
+        <Gallery images={logement.pictures} />
+        <div className="appartment-details">
+          <div>
+            <h1 className="appartment-title">{logement.title}</h1>
+            <span className="location-text">{logement.location}</span>
+            <div className="tag-list">
+              {logement.tags.map((tag, index) => (
+                <Tag title={tag} key={index} />
+              ))}
+            </div>
+
+          </div>
+          <div className="host-and-rate">
+            <Host name={logement.host.name} img={logement.host.picture} />
+            <Rate numStars={logement.rating} />
+          </div>
         </div>
-        
-      </div>
-      <div className="host-and-rate">
-          <Host name={appartment.host.name} img={appartment.host.picture} />
-          <Rate numStars={appartment.rating} />
+        <div className="description-equipment">
+          <DropdownCard
+            title="Description"
+            description={logement.description}
+          />
+          <DropdownCard title="Equipement" description={equipments} />             
         </div>
-    </div>
-    <div className="description-equipment">
-        <DropdownCard
-          title="Description"
-          description={appartment.description}
-        />
-        <DropdownCard title="Equipement" description={equipments} />
       </div>
-    </div>
     </main>
+
   );
 }
 
