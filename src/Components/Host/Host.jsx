c401f19370f8bef55ded01fe90532680ad6ece02
Tarek Noubli <tareknoubli@gmail.com>
2023-01-02 13:23:02 +0100

    Update Host.jsx
    add fragment to fix unique key warning from react rendering

--- src/Components/Host/Host.jsx
+++ src/Components/Host/Host.jsx
@@ -1,19 +1,20 @@
 import "./host.css";
+import React from 'react'
 
 function Host({name, img}) {
     const nameParts = name.split(' ');
 
     return (
         <div className="host">
-            {nameParts.map((part, index) => (
-                <>
-                    {part}
-                    {index < nameParts.length - 1 && <br />}
-                </>
-            ))}
-            <img src={img} alt={"photo de " + name}/>
+          {nameParts.map((part, index) => (
+            <React.Fragment key={index}>
+              {part}
+              {index < nameParts.length - 1 && <br />}
+            </React.Fragment>
+          ))}
+          <img src={img} alt={"photo de " + name} />
         </div>
-    )
+      );
 }
 
 export default Host;
