65bd38299268b3a5629d2b74b71e81663039300f
Tarek Noubli <tareknoubli@gmail.com>
2023-01-11 17:47:34 +0100

    Update DropdownCard.jsx

--- src/Components/DropdownCard/DropdownCard.jsx
+++ src/Components/DropdownCard/DropdownCard.jsx
@@ -3,7 +3,7 @@ import "./dropdownCard.css";
 import arrowUp from "../../assets/dropdownCardArrows/arrow-up.svg";
 import arrowDown from "../../assets/dropdownCardArrows/arrow-down.svg";
 
-function DropdownCard({ title, description }) {
+function DropdownCard({ title, description, key}) {
   const [isOpen, setisOpen] = useState(false);
 
   const handleToggle = (e) => {
