57bb42b7dbab5505cfa26deaa8ee05a40c71878b
Tarek Noubli <tareknoubli@gmail.com>
2023-02-02 14:24:51 +0100

    Update Home.jsx
    change img and add real h1 instead of img with text inside

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import image from "../../assets/Home/Section1.png";
+import image from "../../assets/Home/nature-img.png";
 import appartments from "../../data/appartments";
 import Card from "../../Components/Card/Card.jsx";
 import "./home.css";
@@ -6,20 +6,20 @@ import "./home.css";
 function Home() {
   return (
     <main>
-    <div>
-      <img src={image} alt="" className="img-home-header" />
+      <div className="top-img-and-title">
+        <img src={image} alt="" className="img-home-header" />
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
       <section className="cards">
-        { appartments.map((appartement, index) => (
+        {appartments.map((appartement, index) => (
           <Card
             id={appartement.id}
             image={appartement.cover}
             title={appartement.title}
             key={index}
           />
-          
         ))}
       </section>
-    </div>
     </main>
   );
 }
