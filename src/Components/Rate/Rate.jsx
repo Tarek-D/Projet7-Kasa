c537a2e5f1638a7876d216a04372b2e5de1eb908
Tarek Noubli <tareknoubli@gmail.com>
2023-02-14 14:38:27 +0100

    Update Rate.jsx

--- src/Components/Rate/Rate.jsx
+++ src/Components/Rate/Rate.jsx
@@ -5,15 +5,18 @@ import greystar from "../../assets/Rate/star_rate_E3E3E3.svg";
 const Rate = ({ numStars }) => {
   const stars = [];
   for (let i = 0; i < numStars; i++) {
+    // First loop creates red stars based on the "numStars" prop
     stars.push(
         <img key={i} src={redstar} alt="Red star" />
     );
   }
+  // second loop creates the remaining grey stars to complete a total of five stars
   for (let i = numStars; i < 5; i++) {
     stars.push(
         <img key={i} src={greystar} alt="Grey star" />
     );
   }
+  // Return the array of stars 
   return (
     <div>
       {stars}
